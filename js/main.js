1ade44f9032de58d705efd91380c28c4c20b9f23
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-08 15:16:58 +0100

    Finita parte JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-const nomeUtente = prompt();
-const cognomeUtente = prompt();
-const colorePreferitoUtente = prompt();
+const nomeUtente = prompt("Inserisci il tuo nome:");
+const cognomeUtente = prompt("Inserisci il tuo cognome: ");
+const colorePreferitoUtente = prompt("Inserisci il tuo colore preferito: ");
 
-console.log(`${nomeUtente}${cognomeUtente}${colorePreferitoUtente}21`)
+
+let numeroUno = prompt("Inserisci un numero: ");
+while (isNaN(parseFloat(numeroUno))) {
+    numeroUno = prompt("Quello non è un numero! Prova di nuovo: ");
+}
+numeroUno = parseFloat(numeroUno)
+
+let numeroDue = prompt("Inserisci un'altro numero: ");
+while (isNaN(parseFloat(numeroDue))) {
+    numeroDue = prompt("Quello non è un numero! Prova di nuovo: ");
+}
+numeroDue = parseFloat(numeroDue)
+
+
+console.log(`${nomeUtente}${cognomeUtente}${colorePreferitoUtente}${numeroUno/numeroDue}`);
